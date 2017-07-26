2937a26b3ef15e373dc412d2b84c68e658775499
Francois ANDRE <francois.andre@carnavello.fr>
2017-07-26 12:37:31 +0200

    Ajout composant orcid.vue

---
+++ src/aeris-orcid/aeris-orcid.vue
@@ -0,0 +1,56 @@
+<template>
+<span class="aeris-orcid-host">
+</span>
+</template>
+
+
+<script>
+export default {
+  props: {
+  	orcid:  {
+      type: String,
+      default: null
+    }
+},
+  
+   
+  watch: {
+   },
+  
+  created: function () {
+   this.aerisOrcidRequestListener = this.handleOrcidRequest.bind(this) 
+   document.addEventListener('aerisOrcidRequest', this.aerisOrcidRequestListener);
+  },
+  
+   destroyed: function() {
+   document.removeEventListener('aerisOrcidRequest', this.aerisOrcidRequestListener);
+  	this.aerisOrcidRequestListener = null;
+  },
+  
+  computed: {
+ 
+  },
+  
+  mounted: function() {
+   
+  },
+  
+  data () {
+    return {
+    	aerisOrcidRequestListener: null
+    }
+  },
+  
+  methods: {
+  
+  handleOrcidRequest: function() {
+  	console.log("Orcid ?: "+this.orcid);
+  }
+  
+  }
+}
+</script>
+
+<style>
+   
+</style>
