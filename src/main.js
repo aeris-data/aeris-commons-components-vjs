f4980a88f11376c2a2e9561ba107b88e35b8cd24
Francois ANDRE <francois.andre@carnavello.fr>
2017-07-28 14:58:06 +0200

    0.1.9: Mise en place trace chargement des composants

--- src/main.js
+++ src/main.js
@@ -19,12 +19,26 @@ ljs.addAliases({
 	dep: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/document-register-element/1.4.1/document-register-element.js', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min.js']
 })
 ljs.load('dep', function() {
+	
+	if (!window.registredAerisElements) {
+		window.registredAerisElements = [];
+	}
+	console.info("Début registration des custom elements commons")
+	console.info("Registred elements at this time: "+window.registredAerisElements)
 	Vue.customElement('aeris-theme', AerisTheme);
+	window.registredAerisElements.push('aeris-theme')
 	Vue.customElement('aeris-notification', AerisNotification);
+	window.registredAerisElements.push('aeris-notification')
 	Vue.customElement('aeris-notifier', AerisNotifier);
+	window.registredAerisElements.push('aeris-notifier')
 	Vue.customElement('aeris-cartouche', AerisCartouche);
+	window.registredAerisElements.push('aeris-cartouche')
 	Vue.customElement('aeris-spinner', AerisSpinner);
+	window.registredAerisElements.push('aeris-spinner')
 	Vue.customElement('aeris-browser-alert', AerisBrowserAlert);
+	window.registredAerisElements.push('aeris-browser-alert')
 	Vue.customElement('aeris-orcid', AerisOrcid);
+	window.registredAerisElements.push('aeris-orcid')
+	window.registredAerisElements.push('aeris-commons-components-vjs')
 })
 
