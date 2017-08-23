d0675549afc4f3b110e2d0a787dff34c258e64b3
Francois ANDRE <francois.andre@carnavello.fr>
2017-08-23 10:07:56 +0200

    0.1.14

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ import AerisDatepicker  from './aeris-datepicker/aeris-datepicker.vue'
 
 
 ljs.addAliases({
-	dep: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/document-register-element/1.4.1/document-register-element.js', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min.js','https://cdnjs.cloudflare.com/ajax/libs/moment-range/3.0.3/moment-range.min.js']
+	dep: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', 'https://rawgit.com/aeris-data/aeris-commons-fonts/develop/font/aeriscommmonsfonts.css', 'https://cdnjs.cloudflare.com/ajax/libs/document-register-element/1.4.1/document-register-element.js', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min.js','https://cdnjs.cloudflare.com/ajax/libs/moment-range/3.0.3/moment-range.min.js']
 })
 ljs.load('dep', function() {
 	
