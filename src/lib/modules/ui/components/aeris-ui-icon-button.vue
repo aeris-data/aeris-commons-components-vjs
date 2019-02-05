d56c8388555ef77fd37adf52871d024394335e0d
lvu <le-hung.vu@obs-mip.fr>
2019-02-05 13:49:29 +0100

    refactor(icon-button): refactoring

---
+++ src/lib/modules/ui/components/aeris-ui-icon-button.vue
@@ -0,0 +1,125 @@
+<template>
+  <div aeris-ui-icon-button>
+    <button
+      :style="getTheme"
+      :class="[type, 'tooltip']"
+      :title="title"
+      type="button"
+      aria-hidden="true"
+      @click.prevent="handleClick"
+    >
+      <i v-if="icon" :style="getIconTheme" :class="['fa', icon]" /> {{ text }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "aeris-ui-icon-button",
+
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    icon: {
+      type: String,
+      default: ""
+    },
+    theme: {
+      type: Object,
+      default: null
+    },
+    iconTheme: {
+      type: Object,
+      default: null
+    },
+    type: {
+      type: String,
+      default: "button"
+    },
+    text: {
+      type: String,
+      default: ""
+    }
+  },
+
+  computed: {
+    getTheme() {
+      if (this.theme) {
+        let color = this.theme.color;
+        let emphasis = this.theme.emphasis;
+        return {
+          color: color,
+          background: emphasis
+        };
+      } else {
+        return "";
+      }
+    },
+    getIconTheme() {
+      if (this.iconTheme) {
+        let color = this.iconTheme.color;
+        let emphasis = this.iconTheme.emphasis;
+        return {
+          color: color,
+          background: emphasis
+        };
+      } else {
+        return "";
+      }
+    }
+  },
+
+  methods: {
+    handleClick() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
+
+<style scoped>
+[aeris-ui-icon-button] .icon-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  margin: 0;
+  border: none;
+  outline: none;
+  border-radius: 50%;
+  width: 42px;
+  height: 42px;
+  background: #e2e2e2;
+  color: #333;
+  font-size: 1rem;
+  cursor: pointer;
+}
+
+[aeris-ui-icon-button] .button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  outline: none;
+  border-radius: 0.5rem;
+  padding: 16px;
+  background: #ddd;
+  color: #444;
+  font-size: 1rem;
+  cursor: pointer;
+}
+
+[aeris-ui-icon-button] .button i {
+  margin-right: 10px;
+}
+
+[aeris-ui-icon-button] button::-moz-focus-inner {
+  border: 0;
+}
+
+[aeris-ui-icon-button] button:hover {
+  filter: brightness(80%);
+}
+</style>
---
+++ src/lib/modules/ui/index.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+
+import AerisUiIconButton from "./components/aeris-ui-icon-button-register";
+
+Vue.use(AerisUiIconButton);
